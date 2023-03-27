596a7b15dc1f2b2cc43c00b72bdd9cf30cfc2f7a
baw <otting.8@osu.edu>
2023-03-27 14:57:08 -0500

    improve(fac2023): updated graph for task 3 of challenge pt1

--- financial-analyst-course-2023/challenge-pt-1.py
+++ financial-analyst-course-2023/challenge-pt-1.py
@@ -1,5 +1,7 @@
+import calendar
 import operator
 from contextlib import contextmanager
+from datetime import datetime
 from pathlib import Path
 from typing import Dict, List, Union
 
@@ -206,79 +208,217 @@ for c in clients:
                 )
             raise ae
 
-cmap = sns.color_palette("mako", len(clients))
-plt.figure(figsize=(10, 8))
-ax = plt.subplot(111)
 
-# The dictionary will either have the period as a string or the monthly percentage as a float
-previous_clients: List[Dict[str, Union[str, float]]] = []
-clients = sorted(clients)
-for color_idx, client_name in enumerate(clients):
-    client_data = (
-        monthly_rev_by_client.filter(pl.col("Client name") == client_name)
-        .select(["Period", "Monthly Revenue Percentage"])
-        .to_dict()
-    )
+def plot_client_monthly_revenue_stacked_bar_chart():
+    cmap = sns.color_palette("mako", len(clients))
+    plt.figure(figsize=(10, 8))
+    ax = plt.subplot(111)
 
-    def compute_bottom_percentages(
-        pc: List[Dict[str, Union[str, float]]]
-    ) -> List[float]:
-        if len(pc) == 0:
-            return None
-        elif len(pc) == 1:
-            return pc[0]["Monthly Revenue Percentage"].to_list()
-        else:
-            from collections import Counter
+    # The dictionary will either have the period as a string or the monthly percentage as a float
+    previous_clients: List[Dict[str, Union[str, float]]] = []
+    sorted_clients = sorted(clients)
+    for color_idx, client_name in enumerate(sorted_clients):
+        client_data = (
+            monthly_rev_by_client.filter(pl.col("Client name") == client_name)
+            .select(["Period", "Monthly Revenue Percentage"])
+            .to_dict()
+        )
 
-            counter = Counter()
-            for client in pc:
-                client_values = pl.from_dict(client).to_dicts()
-                client_values = {
-                    cv_row["Period"]: cv_row["Monthly Revenue Percentage"]
-                    for cv_row in client_values
-                }
-                counter.update(client_values)
+        def compute_bottom_percentages(
+            pc: List[Dict[str, Union[str, float]]]
+        ) -> List[float]:
+            if len(pc) == 0:
+                return None
+            elif len(pc) == 1:
+                return pc[0]["Monthly Revenue Percentage"].to_list()
+            else:
+                from collections import Counter
 
-                """"
-                from functools import reduce
-                from operator import add
+                counter = Counter()
+                for client in pc:
+                    client_values = pl.from_dict(client).to_dicts()
+                    client_values = {
+                        cv_row["Period"]: cv_row["Monthly Revenue Percentage"]
+                        for cv_row in client_values
+                    }
+                    counter.update(client_values)
 
-                # print(reduce(add, map(Counter, pc)))
-                TODO: This would be cool to try and implement
-                print(
-                    reduce(
-                        add,
-                        map(lambda k: k["Monthly Revenue Percentage"], client_values),
+                    """"
+                    from functools import reduce
+                    from operator import add
+
+                    # print(reduce(add, map(Counter, pc)))
+                    TODO: This would be cool to try and implement
+                    print(
+                        reduce(
+                            add,
+                            map(lambda k: k["Monthly Revenue Percentage"], client_values),
+                        )
                     )
-                )
-                """
-            return_values = list(counter.values())
-            return return_values
+                    """
+                return_values = list(counter.values())
+                return return_values
 
-    ax.bar(
-        client_data["Period"],
-        client_data["Monthly Revenue Percentage"],
-        bottom=compute_bottom_percentages(previous_clients),
-        color=cmap[color_idx],
+        ax.bar(
+            client_data["Period"],
+            client_data["Monthly Revenue Percentage"],
+            bottom=compute_bottom_percentages(previous_clients),
+            color=cmap[color_idx],
+        )
+        previous_clients.append(client_data)
+
+    # Shrink current axis by 15% and put legend in the space
+    box = ax.get_position()
+    ax.set_position([box.x0, box.y0, box.width * (1 - 0.15), box.height])
+    ax.legend(clients, loc="center left", bbox_to_anchor=(1, 0.5))
+
+    # Change y ticks to percentage values instead of decimals
+    ytl = ax.get_yticklabels()
+    new_ticks = []
+    for tick in ytl:
+        t = tick
+        t._text = f"{float(t._text) * 100:0.0f}%"
+        new_ticks.append(t)
+
+    ax.set_yticklabels(new_ticks)
+    ax.set_xticklabels(month_labels, rotation=40, ha="right", fontsize=8)
+    plt.title("Revenue breakdown per month stacked by client")
+    plt.ylabel("Percent Allocation")
+    plt.xlabel("Month")
+    plt.show()
+
+
+# plot_client_monthly_revenue_stacked_bar_chart()
+
+# So what we are actually looking for is a stacked bar chart broken down by
+# different client types or the `Type of client` column
+# Lets do that below:
+mrev_by_client_type = (
+    df.with_columns(pl.col("Period"))
+    .groupby("Period", "Type of client")
+    .agg(
+        [
+            pl.col(rev_col).sum().alias("Monthly Revenue"),
+        ]
     )
-    previous_clients.append(client_data)
+    .with_columns(
+        pl.col("Period").cast(pl.Date).dt.strftime("%m/%d/%Y"),
+        pl.sum("Monthly Revenue").over(["Period"]).alias("Total Monthly Revenue"),
+    )
+    # TODO: We __might__ be able to put this in the previous `with_columns` statement but
+    # we would have to do it while defining the `Total Monthly Revenue` column in the `struct`
+    # / `apply` function so I chose to just split it into 2 for simplicity
+    .with_columns(
+        pl.struct([pl.col("Monthly Revenue"), pl.col("Total Monthly Revenue")])
+        .apply(lambda x: x["Monthly Revenue"] / x["Total Monthly Revenue"])
+        .alias("Monthly Revenue Percentage"),
+    )
+)
+print(mrev_by_client_type)
+client_types = sorted(
+    mrev_by_client_type.select("Type of client").unique().get_columns()[0].to_list()
+)
 
-# Shrink current axis by 15% and put legend in the space
-box = ax.get_position()
-ax.set_position([box.x0, box.y0, box.width * (1 - 0.15), box.height])
-ax.legend(clients, loc="center left", bbox_to_anchor=(1, 0.5))
 
-# Change y ticks to percentage values instead of decimals
-ytl = ax.get_yticklabels()
-new_ticks = []
-for tick in ytl:
-    t = tick
-    t._text = f"{float(t._text) * 100:0.0f}%"
-    new_ticks.append(t)
+def plot_client_type_monthly_revenue_stacked_bar_chart():
+    cmap = sns.color_palette("mako", len(client_types))
+    plt.figure(figsize=(11, 8))
+    ax = plt.subplot(111)
 
-ax.set_yticklabels(new_ticks)
-ax.set_xticklabels(month_labels, rotation=40, ha="right", fontsize=8)
-plt.title("Revenue breakdown per month stacked by client")
-plt.ylabel("Percent Allocation")
-plt.xlabel("Month")
-plt.show()
+    # The dictionary will either have the period as a string or the monthly percentage as a float
+    previous_types: List[Dict[str, Union[str, float]]] = []
+    for color_idx, client_type in enumerate(client_types):
+        client_type_data = (
+            mrev_by_client_type.filter(pl.col("Type of client") == client_type)
+            .select(["Period", "Monthly Revenue Percentage"])
+            .to_dict()
+        )
+
+        def compute_bottom_percentages(
+            pc: List[Dict[str, Union[str, float]]]
+        ) -> List[float]:
+            if len(pc) == 0:
+                return None
+            elif len(pc) == 1:
+                return pc[0]["Monthly Revenue Percentage"].to_list()
+            else:
+                from collections import Counter
+
+                counter = Counter()
+                for client in pc:
+                    client_values = pl.from_dict(client).to_dicts()
+                    client_values = {
+                        cv_row["Period"]: cv_row["Monthly Revenue Percentage"]
+                        for cv_row in client_values
+                    }
+                    counter.update(client_values)
+
+                    """"
+                    from functools import reduce
+                    from operator import add
+
+                    # print(reduce(add, map(Counter, pc)))
+                    TODO: This would be cool to try and implement
+                    print(
+                        reduce(
+                            add,
+                            map(lambda k: k["Monthly Revenue Percentage"], client_values),
+                        )
+                    )
+                    """
+                return_values = list(counter.values())
+                return return_values
+
+        ax.bar(
+            client_type_data["Period"],
+            client_type_data["Monthly Revenue Percentage"],
+            bottom=compute_bottom_percentages(previous_types),
+            color=cmap[color_idx],
+            width=0.85,
+        )
+        previous_types.append(client_type_data)
+
+    # Shrink current axis by 10% and put legend in the space
+    box = ax.get_position()
+    ax.set_position([box.x0, box.y0, box.width * (1 - 0.10), box.height])
+    ax.legend(client_types, loc="center left", bbox_to_anchor=(1, 0.5))
+
+    # Change y ticks to percentage values instead of decimals
+    ytl = ax.get_yticklabels()
+    new_ticks = []
+    for tick in ytl:
+        t = tick
+        t._text = f"{float(t._text) * 100:0.0f}%"
+        new_ticks.append(t)
+
+    ax.set_yticklabels(new_ticks)
+
+    # Convert the month labels from mm/dd/yyyy to Month-Year
+    converted_labels = []
+    for ml in month_labels:
+        date: datetime = datetime.strptime(ml, "%m/%d/%Y")
+        final_label = f"{calendar.month_abbr[date.month]}-{date.year}"
+        converted_labels.append(final_label)
+    ax.set_xticklabels(converted_labels, rotation=40, ha="right", fontsize=8)
+
+    # Add labels to bars themselves
+    for idx in range(len(ax.containers)):
+        ax.bar_label(
+            ax.containers[idx],
+            labels=[f"{label * 100:0.2f}%" for label in ax.containers[idx].datavalues],
+            label_type="center",
+            fontsize=8,
+            color="white",
+            fontweight="bold",
+        )
+
+    plt.title("Revenue by client type as a percentage of Total Revenues")
+    plt.ylabel("Percent Allocation")
+    plt.xlabel("Month")
+    plt.show()
+
+
+# Create a table with month col and client type row for:
+# 1) Revenue
+# 2) Percent of total revenues
+plot_client_type_monthly_revenue_stacked_bar_chart()
